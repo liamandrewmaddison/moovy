e5ca32f0507e511c46e7ec05eeb03aa25acae0ed
Liam Maddison <liam@signable.co.uk>
2019-03-11 18:03:12 +0000

    Adding basic template for movies module

---
+++ src/store/modules/movies.js
@@ -0,0 +1,16 @@
+
+/* --------------------------------*
+ * STORE/MODULES/MOVIES.JS
+ *
+ * Our store module for our
+ * movie list
+ *--------------------------------*/
+const moviesModule = {
+  namespaced: true,
+  state: { },
+  mutations: { },
+  actions: { },
+  getters: { },
+};
+
+export default moviesModule;
