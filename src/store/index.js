3c9db2757eeccd273ce26c87935b33304e664084
Liam Maddison <liam@signable.co.uk>
2019-03-11 18:06:21 +0000

    Importing movies store module into base vuex store

--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,16 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import moviesModule from '@/store/modules/movies';
 
 Vue.use(Vuex);
 
+/* --------------------------------*
+ * STORE/INDEX.JS
+ *
+ * Our base Vuex store
+ *--------------------------------*/
 export default new Vuex.Store({
-  state: {
-
-  },
-  mutations: {
-
-  },
-  actions: {
-
+  modules: {
+    moviesModule,
   },
 });
