c111a8a89851f58cd0cdd0c8b17486c74f407302
Liam Maddison <liam@signable.co.uk>
2019-03-11 17:49:58 +0000

    Adding api config file

---
+++ src/config/api.js
@@ -0,0 +1,8 @@
+/* --------------------------------*
+ * API.JS
+ *
+ * All of your api configurations
+ *--------------------------------*/
+export default {
+  key: 'cf8c3181778f38b4190468b080bfe571',
+};
