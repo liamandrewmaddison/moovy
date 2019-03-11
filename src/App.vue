41203109fc35b949b7fb87f5ab55f0c63b8b1334
Liam Maddison <liam@signable.co.uk>
2019-03-11 19:24:15 +0000

    Adding Header component and using it in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,23 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <v-header>
+      <h1 slot="main">Moovy!</h1>
+      <h3 slot="sub">Making Film Groovy Again</h3>
+    </v-header>
+    <main></main>
   </div>
 </template>
 
+<script>
+import VHeader from '@/components/Header.vue';
+
+export default {
+  components: {
+    VHeader,
+  },
+};
+</script>
+
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+  @import 'styles/manifest.scss';
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+  <header class="header">
+    <slot name="main" />
+    <slot name="sub" />
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'v-header',
+};
+</script>
+
+<style lang="scss" scoped>
+  @import '../styles/_settings.scss';
+
+  .header {
+    padding: $spacing-base;
+    background-color: $primary-color;
+    color: #fff;
+    h1 {
+      border: 3px solid #fff;
+      width: 250px;
+      margin: 0 auto;
+      line-height: 1;
+      border-radius: 30px;
+      padding: 2px;
+      background: $color-blue-dark;
+    }
+    h3 {
+      margin: ($spacing-base + $spacing-xs) 0 0 0;
+    }
+  }
+</style>
