ba9740ce579a66d86bd43bf7ceb16ad9d5877434
Liam Maddison <liam@signable.co.uk>
2019-03-12 02:25:21 +0000

    removing magicgrid

--- src/main.js
+++ src/main.js
@@ -1,10 +1,7 @@
 import Vue from 'vue';
-import MagicGrid from 'vue-magic-grid';
 import App from './App.vue';
 import store from './store/index';
 import './registerServiceWorker';
-
-Vue.use(MagicGrid);
 
 Vue.config.productionTip = false;
 
