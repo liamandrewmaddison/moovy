fcbe40825351efccd76631fe862e7eeb02660d4a
Liam Maddison <liam@signable.co.uk>
2019-03-12 00:45:48 +0000

    Adding new v-card component and running mapped actions from the store

--- src/App.vue
+++ src/App.vue
@@ -4,16 +4,48 @@
       <h1 slot="main">Moovy!</h1>
       <h3 slot="sub">Making Film Groovy Again</h3>
     </v-header>
-    <main></main>
+    <main class="wrapper" v-show="results.length > 0">
+      <magic-grid>
+        <v-card
+          v-for="result in results"
+          :title="result.original_title"
+          :src="result.poster_path"
+          :genreIds="result.genre_ids"
+          :animate="true"
+          :key="result.id" />
+      </magic-grid>
+    </main>
   </div>
 </template>
 
 <script>
+import { mapActions, mapState } from 'vuex';
+import VCard from '@/components/Card.vue';
 import VHeader from '@/components/Header.vue';
 
+/**
+ * TODO: Add loading state
+ */
 export default {
   components: {
+    VCard,
     VHeader,
+  },
+  async mounted() {
+    await this.getGenres();
+    await this.getMovies();
+  },
+  computed: {
+    ...mapState({
+      results: state => state.movies.results,
+    }),
+  },
+  methods: {
+    // re-names actions for a more succinct API
+    ...mapActions({
+      getMovies: 'movies/GET_MOVIES',
+      getGenres: 'movies/GET_GENRES',
+    }),
   },
 };
 </script>
