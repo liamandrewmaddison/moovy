8605243ebcd00d3a5c06a355007f2de5297ba384
Liam Maddison <liam@signable.co.uk>
2019-03-12 00:41:18 +0000

    Importing MagicGrid

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue';
+import MagicGrid from 'vue-magic-grid';
 import App from './App.vue';
 import store from './store/index';
 import './registerServiceWorker';
+
+Vue.use(MagicGrid);
 
 Vue.config.productionTip = false;
 
