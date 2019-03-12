3b4ea94009afc62e7f21b779f9fff2e1719c2982
Liam Maddison <liam@signable.co.uk>
2019-03-12 00:45:02 +0000

    Adding actions, GET_MOVIES & GET_GENRES and a couple getters to simplify data

--- src/store/modules/movies.js
+++ src/store/modules/movies.js
@@ -1,3 +1,12 @@
+/* eslint no-param-reassign:
+  ["error", {
+    "props": true,
+    "ignorePropertyModificationsFor": ["state"]
+  }
+] */
+
+import http from '../../utils/http';
+
 /* --------------------------------*
  * STORE/MODULES/MOVIES.JS
  *
@@ -10,10 +19,63 @@
 
 const moviesModule = {
   namespaced: true,
-  state: { },
-  mutations: { },
-  actions: { },
-  getters: { },
+  state: {
+    results: [],
+    genres: [],
+  },
+  mutations: {
+    SET_RESULTS(state, { results }) {
+      state.results = results;
+    },
+    SET_GENRES(state, { genres }) {
+      state.genres = genres;
+    },
+  },
+  actions: {
+    async GET_MOVIES({ commit }) {
+      return http({
+        url: 'discover/movie?page=1',
+        params: {
+          sort_by: 'popularity.desc',
+          include_adult: false,
+          include_video: false,
+        },
+      }).then((response) => {
+        const { results } = response.data;
+        commit('SET_RESULTS', { results });
+      });
+    },
+    async GET_GENRES({ commit }) {
+      return http({
+        url: 'genre/movie/list',
+      }).then((response) => {
+        const { genres } = response.data;
+        commit('SET_GENRES', { genres });
+      });
+    },
+  },
+  getters: {
+    availableGenreList(state, getters) {
+      const allGenres = new Set();
+      state.results.forEach((result) => {
+        allGenres.add(...result.genre_ids);
+      });
+
+      return [...allGenres].map(genreId => getters.getGenreById(genreId));
+    },
+    getGenreById: state => id => state.genres.filter(genre => genre.id === id)[0],
+    getGenresByIds: (state, getters) => (ids) => {
+      const genres = new Set();
+
+      state.genres.forEach(() => {
+        ids.forEach((genreId) => {
+          genres.add(getters.getGenreById(genreId));
+        });
+      });
+
+      return genres;
+    },
+  },
 };
 
 export default moviesModule;
