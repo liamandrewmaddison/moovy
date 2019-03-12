f2a5063f5c3e4d10e32b9074fc5927f8cdeef3ac
Liam Maddison <liam@signable.co.uk>
2019-03-12 00:40:31 +0000

    Adding base api_key and language properties

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,9 +1,21 @@
 import axios from 'axios';
+import config from '@/config/api';
 
+/* --------------------------------*
+ * UTILS/HTTP.JS
+ *
+ * Our axios wrapper so that we can
+ * keep our code dry and only
+ * pass what's necessary at all times
+ *--------------------------------*/
 const Http = options => axios({
   method: options.method || 'get',
-  url: `${options.url}`,
-  params: options.params,
+  url: `https://api.themoviedb.org/3/${options.url}`,
+  params: {
+    ...options.params,
+    api_key: config.key,
+    language: 'en-US',
+  },
 });
 
 export default Http;
